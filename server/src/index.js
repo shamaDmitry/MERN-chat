65c33d25c0f285e9c775a62a93a0f0246d5c60c8
shama <shama.dmitry@gmail.com>
2025-02-25 20:36:17 +0200

    fix build

--- server/src/index.js
+++ server/src/index.js
@@ -21,10 +21,10 @@ app.use("/test", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "public", "index.html"));
   });
 }
 
