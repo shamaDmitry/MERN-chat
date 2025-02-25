aaa5dfd2529e75748bb1dc71629e092f1153301f
shama <shama.dmitry@gmail.com>
2025-02-25 17:05:47 +0200

    setup for build

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,14 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectDB from "./lib/db.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,7 +20,13 @@ app.use("/", (req, res) => {
 });
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
