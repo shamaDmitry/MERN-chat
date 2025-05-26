76f68f4670245ad9a5d7867660cb54f1735547cd
shama <shama.dmitry@gmail.com>
2025-05-26 16:28:11 +0300

    add proxy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  // baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `/api`,
   withCredentials: true,
 });
