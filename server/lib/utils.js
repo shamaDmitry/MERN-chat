b46975cbedd01b87d6665989fa3f217913f3d8a5
shama <shama.dmitry@gmail.com>
2025-05-27 16:01:52 +0300

    fix cookies

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -8,8 +8,9 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
+    secure: true,
+    path: "/",
   });
 
   return token;
